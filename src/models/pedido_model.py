4d009f93087976db2b5c3bdca03ba838a71bf8fe
Timoteo <timoteo@hdinfo-ma.com>
2026-03-13 10:59:09 -0300

    feat: Implementação de novos metodos nos models.

--- src/models/pedido_model.py
+++ src/models/pedido_model.py
@@ -1,4 +1,5 @@
 from src.settings.extensions import db
+from datetime import datetime
 
 class Pedido(db.Model):
     
@@ -7,6 +8,7 @@ class Pedido(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     id_item_pedido=db.Column(db.Integer, db.ForeignKey('itens_pedidos.id'), nullable=False)
     # id_cliente=db.Column(db.Integer)
+    created_at=db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
     total=db.Column(db.Float, nullable=False)
     
     def __init__(self, id_item_pedido, total):
@@ -15,3 +17,10 @@ class Pedido(db.Model):
         
     def __repr__(self):
         return f"Pedido: {self.id_item_pedido} - {self.total}"
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "id_item_pedido": self.id_item_pedido,
+            "total": self.total
+        }
